1a9ada605049cf2794a4bb46c0938cf723602e62
mahesh patil <87996248+Mahi7001@users.noreply.github.com>
2022-12-02 15:12:27 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,7 +2,7 @@
 provider "aws" {
   region = "us-east-2" 
   access_key = "AKIAZOCAQTKAEUUHHPVZ"
-  secret_access_key = "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"
+  secret_key = "qvLvykmgFE+LKA7TtuJwsY7kp4goXxXcFoyVUS1S"
 
 }
 
